31ee3388fbd8ae1bf38d3963232b697650d31fa6
khardigree <kaitlynnhardigree@gmail.com>
2024-06-17 21:06:01 -0400

    Set up files, downloaded dependencies and started laying out questions in index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+//dependencies 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const colors = require('colors');
+const { type } = require('os');
+//add shapes 
+//add inputs 
+
+
+//User input questions
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What do you want your logo to say?',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape do you want your logo to be?',
+        choices: ['square', 'circle', 'triangle'],
+    }
+    {
+        type: 'input',
+        name: 'color',
+        message: 'What color do you want your logo to be? Please enter a color name or hex code.',
+        //add validation for color name or hex code
+    }
+];
+
+//initialize the app
